43b127a04607ba4b44839f4bc0c5871b795cb7f8
Fred Zhou <fredpaw@gmail.com>
2020-08-27 23:42:58 +1000

    add navigation and header button

---
+++ navigations/PlacesNavigation.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import PlacesListScreen from "../screens/PlacesListScreen";
+import PlaceDetailScreen from "../screens/PlaceDetailScreen";
+import NewPlaceScreen from "../screens/NewPlaceScreen";
+import MapScreen from "../screens/MapScreen";
+import Colors from "../constants/Colors";
+import HeaderButton from "../components/HeaderButton";
+
+const PlacesStack = createStackNavigator();
+
+const PlacesNavigator = ({ navigation }) => {
+  return (
+    <NavigationContainer>
+      <PlacesStack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+          },
+          headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+        }}
+      >
+        <PlacesStack.Screen
+          name="Places"
+          component={PlacesListScreen}
+          options={({ navigation }) => ({
+            headerTitle: "All Places",
+
+            headerRight: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Add Place"
+                  iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+                  onPress={() => navigation.navigate("NewPlace")}
+                />
+              </HeaderButtons>
+            ),
+          })}
+        />
+        <PlacesStack.Screen name="PlaceDetail" component={PlaceDetailScreen} />
+        <PlacesStack.Screen name="NewPlace" component={NewPlaceScreen} />
+        <PlacesStack.Screen name="Map" component={MapScreen} />
+      </PlacesStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default PlacesNavigator;
---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../components/HeaderButton";
+
+const PlacesListScreen = () => {
+  return (
+    <View>
+      <Text>PlacesListScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default PlacesListScreen;
